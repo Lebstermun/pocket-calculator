d81f2b32d98a638b24bc8ed68bdf6f49fdd28333
Will Harder <willharder1512@gmail.com>
2019-01-07 13:23:56 -0500

    finished displaying numbers

--- calculatorFunc.js
+++ calculatorFunc.js
@@ -1,7 +1,17 @@
+function allClearButton() {
+  let allClearButton= " ";
+  var p=document.getElementById("calcOutput");
+  p.innerHTML=`${allClearButton}`;
+}
 function negativeButton() {
   let negativeButton= "(-)";
   var p=document.getElementById("calcOutput");
   p.innerHTML=`${negativeButton}`;
+}
+function percentButton() {
+  let percentButton= "%";
+  var p=document.getElementById("calcOutput");
+  p.innerHTML=`${percentButton}`;
 }
 function divideButton() {
   let divideButton= "/";
@@ -13,3 +23,76 @@ function oneButton() {
   var p=document.getElementById("calcOutput");
   p.innerHTML=`${oneButton}`;
 }
+function twoButton() {
+  let twoButton= 2;
+  var p=document.getElementById("calcOutput");
+  p.innerHTML=`${twoButton}`;
+}
+function threeButton() {
+  let threeButton= 3;
+  var p=document.getElementById("calcOutput");
+  p.innerHTML=`${threeButton}`;
+}
+function multiplyButton() {
+  let multiplyButton= "x";
+  var p=document.getElementById("calcOutput");
+  p.innerHTML=`${multiplyButton}`;
+}
+function fourButton() {
+  let fourButton= 4;
+  var p=document.getElementById("calcOutput");
+  p.innerHTML=`${fourButton}`;
+}
+function fiveButton() {
+  let fiveButton= 5;
+  var p=document.getElementById("calcOutput");
+  p.innerHTML=`${fiveButton}`;
+}
+function sixButton() {
+  let sixButton= 6;
+  var p=document.getElementById("calcOutput");
+  p.innerHTML=`${sixButton}`;
+}
+function minusButton() {
+  let minusButton= "-";
+  var p=document.getElementById("calcOutput");
+  p.innerHTML=`${minusButton}`;
+}
+function sevenButton() {
+  let sevenButton= 7;
+  var p=document.getElementById("calcOutput");
+  p.innerHTML=`${sevenButton}`;
+}
+function eightButton() {
+  let eightButton= 8;
+  var p=document.getElementById("calcOutput");
+  p.innerHTML=`${eightButton}`;
+}
+function nineButton() {
+  let nineButton= 9;
+  var p=document.getElementById("calcOutput");
+  p.innerHTML=`${nineButton}`;
+}
+function plusButton() {
+  let plusButton= "+";
+  var p=document.getElementById("calcOutput");
+  p.innerHTML=`${plusButton}`;
+}
+  function zeroButton() {
+    let zeroButton= 0;
+    var p=document.getElementById("calcOutput");
+    p.innerHTML=`${zeroButton}`;
+  }
+  function decimalButton() {
+    let decimalButton= ".";
+    var p=document.getElementById("calcOutput");
+    p.innerHTML=`${decimalButton}`;
+  }
+  function equalsButton() {
+    let equalsButton= "=";
+    var p=document.getElementById("calcOutput");
+    p.innerHTML=`${equalsButton}`;
+  }
+  .num{
+    
+  }
